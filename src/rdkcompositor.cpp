10bb0cdd78687279f950725d4e4d2e8a5ed925ee
fkaruv863_comcast <FASIL_KARUVEETTIL@comcast.com>
2026-08-11 16:10:39 -0400

    RDKEMW-23189: Add exception handling to listener callbacks

--- src/rdkcompositor.cpp
+++ src/rdkcompositor.cpp
@@ -1041,12 +1041,22 @@ namespace RdkWindowManager
              }
          }
 
+         LOGINFO("broadcastInputEvent: inputEvent.deviceId=%d, inputEvent.timestamp=%llu, inputEvent.type=%d", inputEvent.deviceId, inputEvent.timestamp, inputEvent.type);
+
          // Do not hold mInputLock while calling listeners.
          for (const auto &listener : listenersCopy) {
              if (listener) {
-                 listener(inputEvent);
+                 try {
+                     listener(inputEvent);
+                 } catch (const std::exception& e) {
+                     RdkWindowManager::Logger::log(LogLevel::Warning, "Exception in input event listener: %s", e.what());
+                 } catch (...) {
+                     RdkWindowManager::Logger::log(LogLevel::Warning, "Unknown exception in input event listener");
+                 }
              }
          }
+
+         Logger::log(LogLevel::Information,  "input metadata sent for device: %d", inputEvent.deviceId);
      }
 
     int RdkCompositor::registerStateChangeEventListener(std::function<void(uint32_t)> listener)
@@ -1104,7 +1114,13 @@ namespace RdkWindowManager
          // Do not hold mStateChangeLock while calling listeners.
          for (const auto &listener : listenersCopy) {
              if (listener) {
-                 listener(state);
+                 try {
+                     listener(state);
+                 } catch (const std::exception& e) {
+                     Logger::log(LogLevel::Warning, "Exception in state change listener: %s", e.what());
+                 } catch (...) {
+                     Logger::log(LogLevel::Warning, "Unknown exception in state change listener");
+                 }
              }
          }
      }
